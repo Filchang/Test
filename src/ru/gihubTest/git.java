509379a32f848c0f5ee3fdfc3c3a104541d65c54
Filch <vlad.filchugov2004@mail.ru>
2024-09-24 22:50:48 +0300

    change git

--- src/ru/gihubTest/git.java
+++ src/ru/gihubTest/git.java
@@ -4,8 +4,9 @@ public class git {
     public static void main(String[] args) {
 
         sum s = new sum(15,29);
+        minus m = new minus(1,32);
 
         System.out.println(s.summary());
+        System.out.println(m.mi());
     }
 }
-status
