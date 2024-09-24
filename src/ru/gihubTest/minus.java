1c4f8ae08a3e3af401dd91542b4859a60b13c59b
Filch <vlad.filchugov2004@mail.ru>
2024-09-24 22:48:21 +0300

    new class minus

---
+++ src/ru/gihubTest/minus.java
@@ -0,0 +1,19 @@
+package ru.gihubTest;
+
+public class minus {
+
+
+        Integer a;
+        Integer b;
+
+        public minus(Integer a, Integer b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public Integer mi()
+        {
+            return a-b;
+        }
+}
