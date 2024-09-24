bf33659568f4b7c7e8700272f4287b7fea2240d0
Filch <vlad.filchugov2004@mail.ru>
2024-09-24 22:29:59 +0300

    new class

---
+++ src/ru/gihubTest/sum.java
@@ -0,0 +1,18 @@
+package ru.gihubTest;
+
+public class sum {
+    Integer a;
+    Integer b;
+
+    public sum(Integer a, Integer b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Integer summary()
+    {
+        return a+b;
+    }
+
+}
